ea25dc8e3fd972767c086388e8f9185338aea5a3
Md shadhin <mdshadhinkn@gmail.com>
2021-04-26 23:38:02 +0600

    menu add

--- src/ts/best.ts
+++ src/ts/best.ts
@@ -1,10 +1,39 @@
-class SvgRange {
-    constructor(element:Element){
-        element.setAttribute("hidden", "true");
+class HumbagerMenu{
+    private isopen: boolean;
+    constructor(private menuIcon: Element, private moreElement?:Array<Element>){
+        this.hadleClick = this.hadleClick.bind(this);
+        this.isopen = this.menuIcon.classList.contains("open");
+        this.menuIcon.addEventListener("click", this.hadleClick);
+    }
+    private hadleClick(){
+        if(this.isopen)
+        {
+            this.close();
+        }
+        else
+        {
+            this.open();
+        }
+    }
+    private open() {
+        this.menuIcon.classList.toggle("open");
+        this.moreElement?.forEach(element=>{
+            element.classList.toggle("open");
+        });
+        this.isopen = true;
+    }
+
+    private close() {
+        this.menuIcon.classList.toggle("open");
+        this.moreElement?.forEach(element=>{
+            element.classList.toggle("open");
+        });
+        this.isopen = false;
     }
 }
 
-var elms: NodeListOf<Element> = document.querySelectorAll(".foo");
-elms.forEach((elem)=>{
-    new SvgRange(elem);
+var menus = document.querySelectorAll(".hambaga");
+
+menus.forEach(menus=>{
+    new HumbagerMenu(menus);
 })
--- static/js/tsc.js
+++ static/js/tsc.js
@@ -1,12 +1,40 @@
 "use strict";
-var SvgRange = (function () {
-    function SvgRange(element) {
-        element.setAttribute("hidden", "true");
+var HumbagerMenu = (function () {
+    function HumbagerMenu(menuIcon, moreElement) {
+        this.menuIcon = menuIcon;
+        this.moreElement = moreElement;
+        this.hadleClick = this.hadleClick.bind(this);
+        this.isopen = this.menuIcon.classList.contains("open");
+        this.menuIcon.addEventListener("click", this.hadleClick);
     }
-    return SvgRange;
+    HumbagerMenu.prototype.hadleClick = function () {
+        if (this.isopen) {
+            this.close();
+        }
+        else {
+            this.open();
+        }
+    };
+    HumbagerMenu.prototype.open = function () {
+        var _a;
+        this.menuIcon.classList.toggle("open");
+        (_a = this.moreElement) === null || _a === void 0 ? void 0 : _a.forEach(function (element) {
+            element.classList.toggle("open");
+        });
+        this.isopen = true;
+    };
+    HumbagerMenu.prototype.close = function () {
+        var _a;
+        this.menuIcon.classList.toggle("open");
+        (_a = this.moreElement) === null || _a === void 0 ? void 0 : _a.forEach(function (element) {
+            element.classList.toggle("open");
+        });
+        this.isopen = false;
+    };
+    return HumbagerMenu;
 }());
-var elms = document.querySelectorAll(".foo");
-elms.forEach(function (elem) {
-    new SvgRange(elem);
+var menus = document.querySelectorAll(".hambaga");
+menus.forEach(function (menus) {
+    new HumbagerMenu(menus);
 });
 //# sourceMappingURL=tsc.js.map
